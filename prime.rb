151c89c34f13c33d5237bfefd379aba9280aa347
Bperez4197 <bryceperez31@gmail.com>
2017-12-01 08:43:42 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,14 @@
+
+require 'pry'
 def prime?(number)
-  if number.even? && number > 2 || number <= 1
-    return false
+  return false if (number.even? && number > 2) || number <= 1
+
   half_num = number / 2
   x = 3
   while x < half_num
     return false if number % x == 0
     x += 2
   end
+
+  true
 end
