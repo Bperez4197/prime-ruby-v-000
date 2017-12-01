79f9e15788bb31fcd83f5d63f2d257b97731c561
Bperez4197 <bryceperez31@gmail.com>
2017-12-01 08:38:22 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(number)
+  return flase if (number.even? && number > 2) || number <= 1
+
+  half_num = number / 2
+  x = 3
+  while x < half_num
+    return flase if number % x == 0
+    x += 2
+  end
+  true
+end
+
